797df3723235ab72e77db793ddb86df965afe97b
marcocareddu <saldumaccu@tuta.io>
2023-06-05 15:10:35 +0200

    JS - Created Var & Array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 console.log('JS OK');
 
+
+// ? FOR
 // * Phase 1
 // Link DOM element
 const olElement = document.getElementById('list-container');
+
+// Create array
+const shoppingList = ['latte', 'pane', 'pasta'];
+
+// Create var
+let printList;
