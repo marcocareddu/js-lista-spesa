c0f7e30c07cc5c3a1d52d3fe50dcad42f09dd2e0
marcocareddu <saldumaccu@tuta.io>
2023-06-05 15:21:20 +0200

    JS - Added WHILE version

--- js/script.js
+++ js/script.js
@@ -1,7 +1,31 @@
 console.log('JS OK');
 
 
-// ? FOR
+
+// ? FOR *****************************************************************
+// // * Phase 1
+// // Link DOM element
+// const olElement = document.getElementById('list-container');
+
+// // Create array
+// const shoppingList = ['latte', 'pane', 'pasta'];
+
+// // Create var
+// let printList = '';
+
+// // FOR cicle
+// for (let i = 0; i < shoppingList.length; i++) {
+//     printList += `<li>${shoppingList[i]}</li>`;
+// }
+
+// // Print list in DOM
+// olElement.innerHTML = printList;
+
+
+
+
+// ? WHILE *****************************************************************
+
 // * Phase 1
 // Link DOM element
 const olElement = document.getElementById('list-container');
@@ -12,9 +36,13 @@ const shoppingList = ['latte', 'pane', 'pasta'];
 // Create var
 let printList = '';
 
-// FOR cicle
-for (let i = 0; i < shoppingList.length; i++) {
+// WHILE cicle
+let i = 0;
+while (i < shoppingList.length) {
     printList += `<li>${shoppingList[i]}</li>`;
+
+    //Increment index
+    i++
 }
 
 // Print list in DOM
